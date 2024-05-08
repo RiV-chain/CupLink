309b1da14f3b2bb94882a71094b5fd43949ff8e8
Vadym Vikulin <vadym.vikulin@rivchain.org>
2024-05-08 19:55:52 +0300

    Implemented foreground notifications

--- app/src/main/kotlin/org/rivchain/cuplink/CallService.kt
+++ app/src/main/kotlin/org/rivchain/cuplink/CallService.kt
@@ -1,10 +1,33 @@
 package org.rivchain.cuplink
 
+import android.app.Notification
 import android.app.Service
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 
+
 class CallService: Service() {
+
+    private val ID_ONGOING_CALL_NOTIFICATION = 201
+
+    var OTHER_NOTIFICATIONS_CHANNEL = 99
+
+    override fun onCreate() {
+        super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val bldr =
+                Notification.Builder(this, OTHER_NOTIFICATIONS_CHANNEL.toString())
+                    .setContentTitle(
+                        getString(
+                            R.string.is_calling
+                        )
+                    )
+                    .setShowWhen(false)
+            bldr.setSmallIcon(R.drawable.ic_audio_device_phone)
+            startForeground(ID_ONGOING_CALL_NOTIFICATION, bldr.build())
+        }
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
         return null;
