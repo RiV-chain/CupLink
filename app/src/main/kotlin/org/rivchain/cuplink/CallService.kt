f5baabf6ea4317eaf58175c0ceb01115b5bd6ef4
Vadym Vikulin <vadym.vikulin@rivchain.org>
2024-05-09 14:05:33 +0300

    Implemented foreground notifications

--- app/src/main/kotlin/org/rivchain/cuplink/CallService.kt
+++ app/src/main/kotlin/org/rivchain/cuplink/CallService.kt
@@ -34,6 +34,29 @@ class CallService : Service() {
     override fun onCreate() {
         notifyServiceReceiver = NotifyServiceReceiver()
         super.onCreate()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val bldr =
+                Notification.Builder(this, OTHER_NOTIFICATIONS_CHANNEL.toString())
+                    .setContentTitle(
+                        getString(
+                            R.string.is_calling
+                        )
+                    )
+                    .setShowWhen(false)
+            bldr.setSmallIcon(R.drawable.ic_audio_device_phone)
+
+            val channel = NotificationChannel(
+                OTHER_NOTIFICATIONS_CHANNEL.toString(),
+                "CupLink",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = "CupLink calls channel for foreground service notification"
+            val n = bldr.build()
+            val notificationManager =
+                getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+            startForeground(ID_ONGOING_CALL_NOTIFICATION, n)
+        }
     }
 
     private fun showIncomingNotification(
